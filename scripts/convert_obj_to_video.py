53887046f641d460044893e49c88fb9ff46be46b
Texaser <alexyh824@gmail.com>
2023-10-26 13:56:11 +0800

    update mesh2video

--- scripts/convert_obj_to_video.py
+++ scripts/convert_obj_to_video.py
@@ -7,14 +7,15 @@ parser.add_argument('--dir', default='logs', type=str, help='Directory where obj
 parser.add_argument('--out', default='videos', type=str, help='Directory where videos will be saved')
 args = parser.parse_args()
 
+directory = args.dir
 out = args.out
-os.makedirs(out, exist_ok=True)
+os.makedirs(f'./experiments/{args.dir}/videos', exist_ok=True)
 
-files = glob.glob(f'{args.dir}/*.obj')
+files = glob.glob(f'./experiments/{args.dir}/*.obj')
 for f in files:
     name = os.path.basename(f)
     # first stage model, ignore
-    if name.endswith('_mesh.obj'): 
-        continue
+    # if name.endswith('_mesh.obj'): 
+    #     continue
     print(f'[INFO] process {name}')
-    os.system(f"python -m kiui.render {f} --save_video {os.path.join(out, name.replace('.obj', '.mp4'))} ")
+    os.system(f"python -m kiui.render {f} --save_video {os.path.join('./experiments', directory, 'videos', name.replace('.obj', '.mp4'))} ")
