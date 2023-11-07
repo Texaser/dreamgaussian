5e42ac7063d6bd5d3b2f1b0855d56ee0defbc5f7
Texaser <alexyh824@gmail.com>
2023-11-07 16:20:08 +0800

    implement perpneg

---
+++ scripts/run_mv.sh
@@ -0,0 +1,6 @@
+python main.py --config configs/text.yaml prompt="a golden goblet" save_path=goblet gpu=0
+# python main2.py --config configs/text.yaml prompt="a golden goblet" save_path=goblet gpu=0
+python main.py --config configs/text.yaml prompt="a photo of a hamburger" save_path=hamburger gpu=1
+# python main2.py --config configs/text.yaml prompt="a photo of a hamburger" save_path=hamburger 
+python main.py --config configs/text.yaml prompt="a delicious croissant" save_path=croissant gpu=2
+# python main2.py --config configs/text.yaml prompt="a delicious croissant" save_path=croissant 
